e520552593c1080dc8a6d16d42374270a65d8e15
Amani95-TA <atawalbeh@blessedtreeit.com>
2025-12-03 09:48:25 +0300

    SDK sources

---
+++ GatetoPayOnboardingSDK.xcframework/ios-arm64/GatetoPayOnboardingSDK.framework/Headers/GatetoPayOnboardingSDK.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for Abyan.
+FOUNDATION_EXPORT double AbyanVersionNumber;
+
+//! Project version string for Abyan.
+FOUNDATION_EXPORT const unsigned char AbyanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Abyan/PublicHeader.h>
+
+//#import <Abyan/Abyan.h>
